2d6497263fe17fd27e4d0f5e1202aa146035edb5
Karl Schriek <karl.schriek@bma.network>
2025-05-05 08:30:19 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# module-base
+# Overview
+
+This module provides the Terraform / OpenTofu deployment specs for an Azure resource group.
+
+
+# Example Usage with Snap CD
+
+TODO
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type = string
+  description = "Name of the resource group to deploy"
+}
+
+variable "location" {
+  type = string
+  description = "Location into which to deploy Azure resources"
+}
